5272a0f7831dac3e450638d50c92296fddb6e1fd
riserstechies <risers.techies@gmail.com>
2018-12-26 18:34:49 +0530

    make it better

---
+++ index.php
@@ -0,0 +1,13 @@
+<DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="1; url=https://www.amazon.com/Arabella-Womens-Pajama-Medium-Heather/dp/B076SMPGV3/ref=lp_18460343011_1_7?s=apparel&ie=UTF8&qid=1545829337&sr=1-7&nodeID=18460343011&psd=1">   
+        <script type="text/javascript">
+               window.location.href = "https://www.amazon.com/Arabella-Womens-Pajama-Medium-Heather/dp/B076SMPGV3/ref=lp_18460343011_1_7?s=apparel&ie=UTF8&qid=1545829337&sr=1-7&nodeID=18460343011&psd=1"
+        </script>
+        <title>Page Redirection</title>
+    </head>
+    <body>
+            </body>
+</html>
